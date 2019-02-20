befb8ab43e08ece84c29c4ebde9554fc41e47433
Fábio Lourenço <1150434@isep.ipp.pt>
2019-02-20 16:17:39 +0000

    Transformation class created

---
+++ COSIG_GW1_1150434_1150625/transformation.hpp
@@ -0,0 +1,46 @@
+//
+//  transformation.hpp
+//  COSIG_GW1_1150434_1150625
+//
+//  Created by Fábio Lourenço on 20/02/2019.
+//  Copyright © 2019 Fábio Lourenço. All rights reserved.
+//
+
+#ifndef transformation_hpp
+#define transformation_hpp
+
+#include <stdio.h>
+#include <iostream>
+
+class Transformation {
+    
+public:
+    double x = 0;
+    double y = 0;
+    double z = 0;
+    
+    double rotationX = 0;
+    double rotationY = 0;
+    double rotationZ = 0;
+    
+    double scaleX = 0;
+    double scaleY = 0;
+    double scaleZ = 0;
+    
+    Transformation();
+    
+    Transformation(
+                   double x,
+                   double y,
+                   double z,
+                   double rotationX,
+                   double rotationY,
+                   double rotationZ,
+                   double scaleX,
+                   double scaleY,
+                   double scaleZ);
+    
+    friend std::ostream& operator<<(std::ostream&, const Transformation&);
+};
+
+#endif /* transformation_hpp */
