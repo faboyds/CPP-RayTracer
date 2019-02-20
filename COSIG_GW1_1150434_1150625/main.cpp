27cb878f3a6d2d137df34639bbe8976e5cad805c
Fábio Lourenço <faboyds@gmail.com>
2019-02-20 13:39:55 +0000

    Initial Commit

---
+++ COSIG_GW1_1150434_1150625/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COSIG_GW1_1150434_1150625
+//
+//  Created by Fábio Lourenço on 20/02/2019.
+//  Copyright © 2019 Fábio Lourenço. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
