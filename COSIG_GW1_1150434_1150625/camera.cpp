f091e353141cdcbef9957d5594cae3489e120d82
1150625_LuisArteiro <1150625@isep.ipp.pt>
2019-02-20 23:10:34 +0000

    Camera is now imported.

---
+++ COSIG_GW1_1150434_1150625/camera.cpp
@@ -0,0 +1,13 @@
+
+#include "camera.hpp"
+
+Camera::Camera(Transformation transformation, double distance, double field_of_view) :
+	transformation(transformation),
+	distance(distance),
+	field_of_view(field_of_view)
+{
+}
+
+std::ostream& operator<<(std::ostream &strm, const Camera &c) {
+	return strm << "Camera( transformation: " << c.transformation << ", distance: " << c.distance << ", field_of_view: " << c.field_of_view << " )";
+}
