766f31eefb8acc9d3411517b1efc064d36d4a957
Fábio Lourenço <faboyds@gmail.com>
2019-03-11 20:02:24 +0000

    Changed transformation_utils to hpp, and added inverse function

--- COSIG_GW1_1150434_1150625/transformation_utils.hpp
+++ COSIG_GW1_1150434_1150625/transformation_utils.hpp
@@ -15,10 +15,10 @@
 
 double transformMatrix[4][4];
 
-void multiply1(double pointA[], double pointB[])
+void multiply1(double const pointA[], double pointB[])
 {
     int i, j;
-    
+
     for (i = 0; i < 4; i++)
         pointB[i] = 0.0;
     for (i = 0; i < 4; i++)
@@ -30,7 +30,7 @@ void multiply3(double matrixA[][4])
 {
     int i, j, k;
     double matrixB[4][4];
-    
+
     for (i = 0; i < 4; i++)
         for (j = 0; j < 4; j++)
         {
@@ -66,7 +66,7 @@ void identityMatrix()
 void translate(double x, double y, double z)
 {
     double translateMatrix[4][4];
-    
+
     translateMatrix[0][0] = 1.0;
     translateMatrix[0][1] = 0.0;
     translateMatrix[0][2] = 0.0;
@@ -89,7 +89,7 @@ void translate(double x, double y, double z)
 void rotateX(double a)
 {
     double rotateXMatrix[4][4];
-    
+
     a *= M_PI / 180.0;
     rotateXMatrix[0][0] = 1.0;
     rotateXMatrix[0][1] = 0.0;
@@ -113,7 +113,7 @@ void rotateX(double a)
 void rotateY(double a)
 {
     double rotateYMatrix[4][4];
-    
+
     a *= M_PI / 180.0;
     rotateYMatrix[0][0] = cos(a);
     rotateYMatrix[0][1] = 0.0;
@@ -137,7 +137,7 @@ void rotateY(double a)
 void rotateZ(double a)
 {
     double rotateZMatrix[4][4];
-    
+
     a *= M_PI / 180.0;
     rotateZMatrix[0][0] = cos(a);
     rotateZMatrix[0][1] = -sin(a);
@@ -161,7 +161,7 @@ void rotateZ(double a)
 void scale(double x, double y, double z)
 {
     double scaleMatrix[4][4];
-    
+
     scaleMatrix[0][0] = x;
     scaleMatrix[0][1] = 0.0;
     scaleMatrix[0][2] = 0.0;
@@ -181,11 +181,140 @@ void scale(double x, double y, double z)
     multiply3(scaleMatrix);
 }
 
+bool inverse(const double m[16], double invOut[16]) {
+    double inv[16], det;
+    int i;
+
+    inv[0] = m[5]  * m[10] * m[15] -
+    m[5]  * m[11] * m[14] -
+    m[9]  * m[6]  * m[15] +
+    m[9]  * m[7]  * m[14] +
+    m[13] * m[6]  * m[11] -
+    m[13] * m[7]  * m[10];
+
+    inv[4] = -m[4]  * m[10] * m[15] +
+    m[4]  * m[11] * m[14] +
+    m[8]  * m[6]  * m[15] -
+    m[8]  * m[7]  * m[14] -
+    m[12] * m[6]  * m[11] +
+    m[12] * m[7]  * m[10];
+
+    inv[8] = m[4]  * m[9] * m[15] -
+    m[4]  * m[11] * m[13] -
+    m[8]  * m[5] * m[15] +
+    m[8]  * m[7] * m[13] +
+    m[12] * m[5] * m[11] -
+    m[12] * m[7] * m[9];
+
+    inv[12] = -m[4]  * m[9] * m[14] +
+    m[4]  * m[10] * m[13] +
+    m[8]  * m[5] * m[14] -
+    m[8]  * m[6] * m[13] -
+    m[12] * m[5] * m[10] +
+    m[12] * m[6] * m[9];
+
+    inv[1] = -m[1]  * m[10] * m[15] +
+    m[1]  * m[11] * m[14] +
+    m[9]  * m[2] * m[15] -
+    m[9]  * m[3] * m[14] -
+    m[13] * m[2] * m[11] +
+    m[13] * m[3] * m[10];
+
+    inv[5] = m[0]  * m[10] * m[15] -
+    m[0]  * m[11] * m[14] -
+    m[8]  * m[2] * m[15] +
+    m[8]  * m[3] * m[14] +
+    m[12] * m[2] * m[11] -
+    m[12] * m[3] * m[10];
+
+    inv[9] = -m[0]  * m[9] * m[15] +
+    m[0]  * m[11] * m[13] +
+    m[8]  * m[1] * m[15] -
+    m[8]  * m[3] * m[13] -
+    m[12] * m[1] * m[11] +
+    m[12] * m[3] * m[9];
+
+    inv[13] = m[0]  * m[9] * m[14] -
+    m[0]  * m[10] * m[13] -
+    m[8]  * m[1] * m[14] +
+    m[8]  * m[2] * m[13] +
+    m[12] * m[1] * m[10] -
+    m[12] * m[2] * m[9];
+
+    inv[2] = m[1]  * m[6] * m[15] -
+    m[1]  * m[7] * m[14] -
+    m[5]  * m[2] * m[15] +
+    m[5]  * m[3] * m[14] +
+    m[13] * m[2] * m[7] -
+    m[13] * m[3] * m[6];
+
+    inv[6] = -m[0]  * m[6] * m[15] +
+    m[0]  * m[7] * m[14] +
+    m[4]  * m[2] * m[15] -
+    m[4]  * m[3] * m[14] -
+    m[12] * m[2] * m[7] +
+    m[12] * m[3] * m[6];
+
+    inv[10] = m[0]  * m[5] * m[15] -
+    m[0]  * m[7] * m[13] -
+    m[4]  * m[1] * m[15] +
+    m[4]  * m[3] * m[13] +
+    m[12] * m[1] * m[7] -
+    m[12] * m[3] * m[5];
+
+    inv[14] = -m[0]  * m[5] * m[14] +
+    m[0]  * m[6] * m[13] +
+    m[4]  * m[1] * m[14] -
+    m[4]  * m[2] * m[13] -
+    m[12] * m[1] * m[6] +
+    m[12] * m[2] * m[5];
+
+    inv[3] = -m[1] * m[6] * m[11] +
+    m[1] * m[7] * m[10] +
+    m[5] * m[2] * m[11] -
+    m[5] * m[3] * m[10] -
+    m[9] * m[2] * m[7] +
+    m[9] * m[3] * m[6];
+
+    inv[7] = m[0] * m[6] * m[11] -
+    m[0] * m[7] * m[10] -
+    m[4] * m[2] * m[11] +
+    m[4] * m[3] * m[10] +
+    m[8] * m[2] * m[7] -
+    m[8] * m[3] * m[6];
+
+    inv[11] = -m[0] * m[5] * m[11] +
+    m[0] * m[7] * m[9] +
+    m[4] * m[1] * m[11] -
+    m[4] * m[3] * m[9] -
+    m[8] * m[1] * m[7] +
+    m[8] * m[3] * m[5];
+
+    inv[15] = m[0] * m[5] * m[10] -
+    m[0] * m[6] * m[9] -
+    m[4] * m[1] * m[10] +
+    m[4] * m[2] * m[9] +
+    m[8] * m[1] * m[6] -
+    m[8] * m[2] * m[5];
+
+    det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
+
+    if (det == 0)
+        return false;
+
+    det = 1.0 / det;
+
+    for (i = 0; i < 16; i++)
+        invOut[i] = inv[i] * det;
+
+    return true;
+}
+
 void process(void)
 {
     int m, i;
     double pointA[4], pointB[4];
-    
+
     while (scanf("%d", &m) != EOF)
     {
         printf("%d\n", m);
