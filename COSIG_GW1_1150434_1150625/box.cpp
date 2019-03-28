565947961b542186a72fe107fddf080c1331dae7
Fábio Lourenço <faboyds@gmail.com>
2019-03-28 00:34:01 +0000

    Added normal calculation to Box

--- COSIG_GW1_1150434_1150625/box.cpp
+++ COSIG_GW1_1150434_1150625/box.cpp
@@ -66,6 +66,39 @@ inline double Box::hit_object(ray &r, vec3 &normal) {
 
     if (tfar < 0) return -1;
 
+    vec3 point = r.point_at_parameter(tnear);
+
+    static const vec3 normals[] = { // A cube has 3 possible orientations
+            vec3(1,0,0),
+            vec3(0,1,0),
+            vec3(0,0,1)
+    };
+
+    const vec3 interRelative = point - vec3(transformation.x, transformation.y, transformation.z);
+    const double xyCoef = interRelative.y() / interRelative.x();
+    const double zyCoef = interRelative.y() / interRelative.z();
+
+    int coef;
+
+    if(xyCoef >= -1 && xyCoef <= 1) {
+        coef = 1;
+    } else if (zyCoef > -1 && zyCoef < 1) {  // Here it's exclusive to avoid coef to be 3
+        coef = 2;
+    } else {
+        coef = 0;
+    }
+
+
+    normal = normals[coef];
+
+    if(coef == 0 && point.x() * 1.000001 >= -maxCoord) {
+        normal = -normal;
+    } else if(coef == 1 && point.y() * 1.000001 >= -maxCoord) {
+        normal = -normal;
+    } else if(coef == 2 && point.z() * 1.000001 >= -maxCoord) {
+        normal = -normal;
+    }
+
     return tnear;
 }
 
